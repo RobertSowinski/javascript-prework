cd094bea0c1119b197c24bef3d3c7bb7fcc97260
Robert Sowiński <saironeandonly@gmail.com>
2025-01-17 12:13:56 +0100

    papier kamien nozyce skonczone, usuniete zmienne var dodane zmienne let

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
-var buttonpaper, buttonrock, buttonscissors;
-/**
- * Describe this function...
- */
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
   if (argMoveId == 1) {
@@ -16,7 +11,6 @@ function getMoveName(argMoveId) {
     return 'kamień';
   }
 }
-
 /**
  * Describe this function...
  */
@@ -36,6 +30,7 @@ function displayResult(argPlayerMove, argComputerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 
+let buttonpaper, buttonrock, buttonscissors;
 buttonscissors = document.getElementById('button-scissors');
 buttonscissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 buttonpaper = document.getElementById('button-paper');
@@ -47,9 +42,9 @@ function buttonClicked(argButtonName) {
     clearMessages();
     console.log(buttonrock + ' został kliknięty');
     playerMove = argButtonName;
-    console.log('wybór ruchu gracza to: ' + playerInput);
+    console.log('wybór ruchu gracza to: ' + playerMove);
     console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
     computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
